bec49fde6b182987481ffc747ab1a3653905010e
ntheanh <tozaco51@gmail.com>
2023-12-09 08:29:00 +0700

    mvc

---
+++ mvc/controllers/Home.php
@@ -0,0 +1,12 @@
+ <?php
+class Home{
+  public static function SayHi(){
+    echo "Home SayHi";
+  }
+  public static function Show(){
+    echo "Home Show";
+  }
+}
+?>
+
+ <!-- Home controller -->
---
+++ mvc/controllers/News.php
@@ -0,0 +1,10 @@
+<?php
+class News{
+  public static function SayHello(){
+    echo "News Sayhello";
+  }
+  public static function abc($ten, $ho){
+    echo $ten."-".$ho;
+  }
+}
+?>
